1956fba94ec9bb7d991c035af1a4d28f993eeb8f
Sondre <songro93@gmail.com>
2017-03-10 09:55:38 +0100

    Wrote Student class

---
+++ OOP-Prac18/Peope/Student.vb
@@ -0,0 +1,18 @@
+﻿Public Class Student
+    Inherits Person
+    Public Property year As Integer
+    Public Property study As String
+
+    Public Sub New(forename As String, surname As String, address As String, dob As Date, email As String, phone As String, year As Integer, study As String)
+        MyBase.New(forename, surname, address, dob, email, phone)
+        Me.year = year
+        Me.study = study
+    End Sub
+
+    Public Overrides Function ToString() As String
+        Return _
+            $"{MyBase.ToString()}, " &
+            $"Study: {study}, " &
+            $"Year: {year}"
+    End Function
+End Class
