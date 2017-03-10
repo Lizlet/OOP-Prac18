eaf18dc7164d6a4687a4624581c6efed5143191b
Sondre <songro93@gmail.com>
2017-03-10 10:29:39 +0100

    Wrote Employee Class

---
+++ OOP-Prac18/Peope/Employee.vb
@@ -0,0 +1,18 @@
+﻿Public Class Employe
+    Inherits Person
+    Public Property position As String
+    Public Property payGrade As UInteger
+
+    Public Sub New(forename As String, surname As String, address As String, dob As Date, email As String, phone As String, position As String, payGrade As UInteger)
+        MyBase.New(forename, surname, address, dob, email, phone)
+        Me.position = position
+        Me.payGrade = payGrade
+    End Sub
+
+    Public Overrides Function ToString() As String
+        Return _
+            $"{MyBase.ToString()}, " &
+            $"Position: {position}, " &
+            $"Pay grade: {payGrade}"
+    End Function
+End Class
