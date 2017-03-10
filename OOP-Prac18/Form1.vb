bdea0c22a126b2130b3fb7fa091669b6f9bf1c8d
Sondre <songro93@gmail.com>
2017-03-10 12:11:09 +0100

    started o nthe random person generator

--- OOP-Prac18/Form1.vb
+++ OOP-Prac18/Form1.vb
@@ -1,3 +1,10 @@
-﻿Public Class Form1
+﻿Imports System.Globalization
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Static rng As System.Random = New System.Random
 
+        For x = 0 To 100
+            Debug.WriteLine(RandomPersonsGenerator.GenerateRandomPerson)
+        Next
+    End Sub
 End Class
---
+++ OOP-Prac18/Peope/RandomPersonsGenerator.vb
@@ -0,0 +1,28 @@
+﻿Imports System.Globalization
+Public Class RandomPersonsGenerator
+    Public Shared Function GenerateRandomPersonsList() As Person()
+
+    End Function
+
+    Public Shared Function GenerateRandomPerson() As Person
+        Static rng As System.Random = New System.Random
+        Dim name As String = My.Resources.namelist.Split(Environment.NewLine)(rng.Next(0, 50))
+        Dim address As String = My.Resources.addresslist.Split(Environment.NewLine)(rng.Next(0, 50))
+        Dim dob As Date = Date.ParseExact(My.Resources.doblist.Split(Environment.NewLine)(rng.Next(1, 51)).Substring(1, 10), "dd/MM/yyyy", CultureInfo.InvariantCulture)
+        Dim email As String = $"{name.Split(" ")(0).Substring(1, name.Split(" ")(0).Length - 1)}.{name.Split(" ")(1)}@gmail.com"
+        Dim phone As String = $"{rng.Next(1000, 9999)} {rng.Next(100000, 999999)}"
+
+        name = name.Substring(1, name.Length - 1)
+        address = address.Substring(1, address.Length - 1)
+
+        Dim checker As Integer = rng.Next(0, 3)
+
+        If checker = 0 Then
+
+        ElseIf checker = 1 Then
+
+        End If
+
+        Return New Person(name.Split(" ")(0), name.Split(" ")(1), address, dob, email, phone)
+    End Function
+End Class
